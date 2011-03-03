419947306cea82efafa827c26fc7f88a940ebac1
Takuya Murakami <tmurakam@tmurakam.org>
2011-03-03 09:54:21 +0900

    newly created ormapper

---
+++ Classes/ormapper/ORRecord.h
@@ -0,0 +1,30 @@
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+#import <UIKit/UIKit.h>
+#import "Database.h"
+
+#define PKEY @"pkey"
+
+@interface ORRecord : NSObject
+{
+    int mPid;
+    BOOL mIsInserted;
+}
+
+@property(nonatomic,assign) int pid;
+
++ (BOOL)migrate:(NSArray *)array;
++ (NSMutableArray *)find_all;
++ (NSMutableArray *)find_cond:(NSString *)cond;
++ (id)find:(int)id;
+
+- (void)save;
+- (void)insert;
+- (void)update;
+- (void)delete;
++ (void)delete_all;
+
++ (NSString *)tableName;
+
+@end
+
+
