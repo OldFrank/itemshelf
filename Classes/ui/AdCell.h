c763fc4a84a5cffa449dfffdcaf2f090a4b4e077
Takuya Murakami <tmurakam@tmurakam.org>
2011-04-27 20:51:31 +0900

    update AdCell for AdMob

--- Classes/ui/AdCell.h
+++ Classes/ui/AdCell.h
@@ -5,27 +5,16 @@
 
 #import <UIKit/UIKit.h>
 
-#import "GADAdViewController.h"
-#import "GADAdSenseParameters.h"
+#import "GADBannerView.h"
 
-#define AFMA_CLIENT_ID  @"ca-mb-app-pub-4621925249922081"
-#define AFMA_APPID @"306480147" // ItemShelf Lite
+#define ADMOB_PUBLISHER_ID      @"a14a925cd4c5c5f" // ItemShelf Lite
 
-#define AFMA_CHANNEL_IDS @"3548046115"
-#define AFMA_CHANNEL_IDS_IPAD @"6436993045"
-#define AFMA_KEYWORDS  @"本,書籍,コミック,CD,DVD,book,comics" // shopping
-#define AFMA_IS_TEST 0
-
-@interface AdCell : UITableViewCell <GADAdViewControllerDelegate> {
-    GADAdViewController *adViewController;
-    UIViewController *parentViewController;
+@interface AdCell : UITableViewCell {
 }
 
 @property(nonatomic,assign) UIViewController *parentViewController;
 
 + (AdCell *)adCell:(UITableView *)tableView parentViewController:(UIViewController *)parentViewController;
 + (CGFloat)adCellHeight;
-
-+ (NSDictionary *)adAttributes;
 
 @end
--- Classes/ui/AdCell.m
+++ Classes/ui/AdCell.m
@@ -4,7 +4,7 @@
 //
 
 // Note:
-//   AdSense : size = 320x50
+//   AdMob : size = 320x50
 
 #import "AdCell.h"
 #import "Common.h"
@@ -14,11 +14,11 @@
 
 @implementation AdCell
 
-@synthesize parentViewController;
+static GADBannerView *sAdBannerView = nil;
 
 + (CGFloat)adCellHeight
 {
-    return 50; // AdSense
+    return 50; // AdMob
 }
 
 + (AdCell *)adCell:(UITableView *)tableView parentViewController:(UIViewController *)parentViewController
@@ -29,51 +29,9 @@
     if (cell == nil) {
         cell = [[[AdCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier] autorelease];
     }
-    cell.parentViewController = parentViewController;
+    sAdBannerView.rootViewController = parentViewController;
 
     return cell;
-}
-
-+ (NSDictionary *)adAttributes
-{
-    NSDictionary *attributes = 
-        [NSDictionary dictionaryWithObjectsAndKeys:
-         AFMA_CLIENT_ID, kGADAdSenseClientID,
-         @"Takuya Murakami", kGADAdSenseCompanyName,
-         @"ItemShelf Lite", kGADAdSenseAppName,
-         AFMA_APPID, kGADAdSenseApplicationAppleID,
-         AFMA_KEYWORDS, kGADAdSenseKeywords,
-         [NSNumber numberWithInt:AFMA_IS_TEST], kGADAdSenseIsTestAdRequest,
-         //[UIColor brownColor], kGADAdSenseAdBackgroundColor,
-         //[UIColor brownColor], kGADAdSenseAdTopBackgroundColor,
-         nil];
-
-    NSMutableDictionary *md = [[[NSMutableDictionary alloc] init] autorelease];
-    [md setDictionary:attributes];
-    
-    if (!IS_IPAD) {
-        [md setObject:[NSArray arrayWithObjects:AFMA_CHANNEL_IDS, nil] forKey:kGADAdSenseChannelIDs];
-        //[md setObject:[UIColor colorWithRed:175/255.0 green:140/255.0 blue:105/256.0 alpha:0.0] forKey:kGADAdSenseAdBackgroundColor];
-        [md setObject:[UIColor whiteColor] forKey:kGADAdSenseAdBackgroundColor];         
-    } else {
-        [md setObject:[NSArray arrayWithObjects:AFMA_CHANNEL_IDS_IPAD, nil] forKey:kGADAdSenseChannelIDs];
-        [md setObject:[UIColor whiteColor] forKey:kGADAdSenseAdBackgroundColor];         
-    }
-        //[UIColor brownColor], kGADAdSenseAdBackgroundColor,
-        //[UIColor colorWithRed:235/255.0 green:205/255.0 blue:180/256.0 alpha:0], kGADAdSenseAdBackgroundColor,
-        //[UIColor colorWithRed:185/255.0 green:145/255.0 blue:113/256.0 alpha:0], kGADAdSenseAdBackgroundColor,
-
-        //[UIColor darkGrayColor], kGADAdSenseAdBackgroundColor,
-
-        //[UIColor lightGrayColor], kGADAdSenseAdBorderColor,
-         
-        //[UIColor blackColor], kGADAdSenseAdTextColor,
-        //[UIColor colorWithRed:0.0 green:0.0 blue:0.5 alpha:0], kGADAdSenseAdTextColor,
-          
-        //[UIColor colorWithRed:0.0 green:0.0 blue:0.5 alpha:0], kGADAdSenseAdLinkColor,
-        //[UIColor colorWithRed:0.0 green:0.4 blue:0.0 alpha:0], kGADAdSenseAdURLColor,
-
-    return md;
 }
 
 - (UITableViewCell *)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)identifier
@@ -81,40 +39,27 @@
     self = [super initWithStyle:style reuseIdentifier:identifier];
 
     // 広告を作成する
-    adViewController= [[GADAdViewController alloc] initWithDelegate:self];
-    adViewController.adSize = kGADAdSize320x50;
-    
-    NSDictionary *attributes = [AdCell adAttributes];
-    [adViewController loadGoogleAd:attributes];
-    
-    UIView *v = adViewController.view;
-    
-    CGRect frame = v.frame;
+    if (sAdBannerView == nil) {
+        CGRect frame = CGRectMake(0, 0, 320, 50);
+        sAdBannerView = [[GADBannerView alloc] initWithFrame:frame];
+        sAdBannerView.adUnitID = ADMOB_PUBLISHER_ID;
+    }
+        
+    CGRect frame = sAdBannerView.frame;
     frame.origin.x = (self.frame.size.width - frame.size.width) / 2;
     frame.origin.y = 0;
-    v.frame = frame;
-    v.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
-    [self.contentView addSubview:v];
+    sAdBannerView.frame = frame;
+    sAdBannerView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
+    [self.contentView addSubview:sAdBannerView];
+
+    // リクエスト開始
+    [sAdBannerView loadRequest:[GADRequest request]];
     
     return self;
 }
 
 - (void)dealloc {
-    [adViewController release];
     [super dealloc];
-}
-
-
-#pragma mark GADAdViewControllerDelegate
-
-- (UIViewController *)viewControllerForModalPresentation:(GADAdViewController *)adController
-{
-    return self.parentViewController;
-}
-
-- (GADAdClickAction)adControllerActionModelForAdClick:(GADAdViewController *)adController
-{
-    return GAD_ACTION_DISPLAY_INTERNAL_WEBSITE_VIEW;
 }
 
 @end
