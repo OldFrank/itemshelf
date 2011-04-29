793e14197c2b0a3c78939634334ce0fa1775c96e
Takuya Murakami <tmurakam@tmurakam.org>
2011-04-30 01:47:49 +0900

    add DropboxSDK / DropboxBackup

---
+++ Classes/DropboxBackup.m
@@ -0,0 +1,157 @@
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+/*
+ * CashFlow for iOS
+ * Copyright (C) 2008-2011, Takuya Murakami, All rights reserved.
+ * For conditions of distribution and use, see LICENSE file.
+ */
+
+#import "DropboxBackup.h"
+#import "Database.h"
+#import "AppDelegate.h"
+
+#define DBNAME @"itemshelf.db"
+#define	BACKUP_FILENAME	DBNAME
+
+#define MODE_BACKUP 0
+#define MODE_RESTORE 1
+
+@implementation DropboxBackup
+
+- (id)init:(id<DropboxBackupDelegate>)delegate
+{
+    self = [super init];
+    if (self) {
+        mDelegate = delegate;
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [mRestClient release];
+    [super dealloc];
+}
+
+- (void)doBackup:(UIViewController *)viewController
+{
+    mMode = MODE_BACKUP;
+    mViewController = viewController;
+    [self _login];
+}
+
+- (void)doRestore:(UIViewController *)viewController
+{
+    mMode = MODE_RESTORE;
+    mViewController = viewController;
+    [self _login];
+}
+
+- (void)unlink
+{
+    DBSession *session = [DBSession sharedSession];
+    if ([session isLinked]) {
+        [session unlink];
+    }
+
+    [self _showResult:@"Your dropbox account has been unlinked"];
+}
+
+- (void)_login
+{
+    DBSession *session = [DBSession sharedSession];
+    
+    // ログイン処理
+    if (![session isLinked]) {
+        // 未ログイン
+        DBLoginController *controller = [[DBLoginController new] autorelease];
+        controller.delegate = self;
+        [controller presentFromController:mViewController];
+    } else {
+        // ログイン済み
+        [self _exec];
+    }
+}
+
+- (void)_exec
+{
+    NSString *dbPath = [[Database instance] dbPath:DBNAME];
+
+    switch (mMode) {
+        case MODE_BACKUP:
+            [self.restClient
+             uploadFile:BACKUP_FILENAME
+             toPath:@"/"
+             fromPath:dbPath];
+            [mDelegate dropboxBackupStarted:NO];
+            break;
+
+        case MODE_RESTORE:
+            // shutdown database
+            [DataModel finalize];
+            [self.restClient loadFile:@"/" BACKUP_FILENAME intoPath:dbPath];
+            [mDelegate dropboxBackupStarted:YES];
+            break;
+    }
+}
+
+- (DBRestClient *)restClient
+{
+    if (mRestClient == nil) {
+    	mRestClient = [[DBRestClient alloc] initWithSession:[DBSession sharedSession]];
+    	mRestClient.delegate = self;
+    }
+    return mRestClient;
+}
+
+#pragma mrk DBRestClientDelegate
+
+// backup finished
+- (void)restClient:(DBRestClient*)client uploadedFile:(NSString*)destPath from:(NSString*)srcPath
+{
+    [self _showResult:@"Backup done."];
+    [mDelegate dropboxBackupFinished];
+}
+
+// backup failed
+- (void)restClient:(DBRestClient*)client uploadFileFailedWithError:(NSError*)error
+{
+    [self _showResult:@"Backup failed!"];
+    [mDelegate dropboxBackupFinished];
+}
+
+// restore done
+- (void)restClient:(DBRestClient*)client loadedFile:(NSString*)destPath
+{
+    [self _showResult:@"Restore done."];
+    [[DataModel sharedDataModel] loadDB];
+    [mDelegate dropboxBackupFinished];
+}
+
+// restore failed
+- (void)restClient:(DBRestClient*)client loadFileFailedWithError:(NSError*)error
+{
+    [self _showResult:@"Restore failed!"];
+    [[DataModel sharedDataModel] loadDB];
+    [mDelegate dropboxBackupFinished];
+}
+
+- (void)_showResult:(NSString *)message
+{
+    [[[[UIAlertView alloc] 
+       initWithTitle:@"Dropbox" message:message
+       delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil]
+        autorelease]
+        show];
+}
+
+#pragma mark DBLoginControllerDelegate methods
+
+- (void)loginControllerDidLogin:(DBLoginController*)controller {
+    [self _exec];
+}
+
+- (void)loginControllerDidCancel:(DBLoginController*)controller {
+    [mDelegate dropboxBackupFinished];
+}
+
+@end
