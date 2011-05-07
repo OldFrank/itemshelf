c0f60e3328adf70fab1d1f21bcf91a39ae1f1737
Takuya Murakami <tmurakam@tmurakam.org>
2011-05-08 00:49:41 +0900

    add new unittest

---
+++ ItemShelfTest/ItemShelfTest.h
@@ -0,0 +1,17 @@
+//
+//  ItemShelfTest.h
+//  ItemShelfTest
+//
+//  Created by 村上 卓弥 on 11/05/08.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface ItemShelfTest : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ ItemShelfTest/ItemShelfTest.m
@@ -0,0 +1,33 @@
+//
+//  ItemShelfTest.m
+//  ItemShelfTest
+//
+//  Created by 村上 卓弥 on 11/05/08.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ItemShelfTest.h"
+
+
+@implementation ItemShelfTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ItemShelfTest");
+}
+
+@end
